e6777f7069c5b74df3a336e97eec6ab7ad05c39f
b7730yejin <b7730yejin@gmail.com>
2022-03-24 10:33:59 +0900

    add Timer And GiguCatchGame code and update some code Name

--- ES6LiteralFormal.js
+++ ES6LiteralFormal.js
@@ -6,9 +6,9 @@ const div = (num1, num2) => num1 / num2;
 const NUM1 = prompt("첫번째 수를 입력하세요");
 const NUM2 = prompt("두번째 수를 입력하세요");
 
-document.write(
+console.log(
   `${NUM1} + ${NUM2} = ${add(NUM1 * 1, NUM2 * 1)} <br/>
-            ${NUM1} - ${NUM2} = ${sub(NUM1, NUM2)} <br/>
+            ${NUM1} - ${NUM2} = ${sub(NUM1, NUM2)} <br/>;
             ${NUM1} * ${NUM2} = ${mul(NUM1, NUM2)} <br/>
             ${NUM1} / ${NUM2} = ${div(NUM1, NUM2)} `
 );
